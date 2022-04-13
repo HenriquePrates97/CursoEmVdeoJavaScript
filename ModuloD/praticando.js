053d559653781d0d2b7a2178c71e26a685984a58
Henrique Prates <henriqueprates@live.com>
2022-04-12 21:43:33 -0300

    continuando modulo d

--- ModuloD/praticando.js
+++ ModuloD/praticando.js
@@ -13,3 +13,57 @@ if(pais == 'Brasil' || pais == 'brasil'){
 }else{
     console.log(`Seu país é ${pais}, logo você é estrangeiro`)
 }
+
+//Condição aninhada
+var idade = 68
+if(idade < 16){
+    console.log('Não vota')
+}else{
+    if(idade < 18 || idade > 67){
+        console.log('Voto opcional')
+    }else{
+        console.log('Voto obrigatório')
+    }
+}
+
+//Bom dia, boa tarde, boa noite
+var agora = new Date() //new Date serve pra buscar as informações do tempo do sistema, como dia, hora, ano, meses, segundos...
+var hora = agora.getHours() //adicionamos a variavel com o date a uma outra variavel e selecionamos o que desejamos pegar
+console.log(`Agora são exatamente ${hora} horas.`)
+if(hora < 12){
+    console.log('Bom dia!!!')
+}else if(hora <=18){
+    console.log('Boa tarde!!!')
+}else{
+    console.log('Boa noite!!!')
+}
+
+//Condição multipla
+var agora = new Date()
+var diaSem = agora.getDay() // 0 = dom, 1 = seg, 2 = ter, 3 = qua, 4 = qui, 5 = sex, 6 = sab
+console.log(diaSem)
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Erro!!! Dia inválido!')
+}
